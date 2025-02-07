ec99a1c9d1909ebd6ce7a5484cf532c4e2f204e2
Philip Fletcher <philip.fletcher@stutchbury.com>
2025-02-07 13:36:11 +0000

    Update API docs

---
+++ docs/api/annotated_dup.js
@@ -0,0 +1,6 @@
+var annotated_dup =
+[
+    [ "AdafruitFT6206TouchScreen", "classAdafruitFT6206TouchScreen.html", "classAdafruitFT6206TouchScreen" ],
+    [ "AdafruitResistiveTouchScreen", "classAdafruitResistiveTouchScreen.html", "classAdafruitResistiveTouchScreen" ],
+    [ "TouchScreenAdapter", "classTouchScreenAdapter.html", "classTouchScreenAdapter" ]
+];
---
+++ docs/api/hierarchy.js
@@ -0,0 +1,8 @@
+var hierarchy =
+[
+    [ "TouchScreenAdapter::TouchPoint", "structTouchScreenAdapter_1_1TouchPoint.html", null ],
+    [ "TouchScreenAdapter", "classTouchScreenAdapter.html", [
+      [ "AdafruitFT6206TouchScreen", "classAdafruitFT6206TouchScreen.html", null ],
+      [ "AdafruitResistiveTouchScreen", "classAdafruitResistiveTouchScreen.html", null ]
+    ] ]
+];
